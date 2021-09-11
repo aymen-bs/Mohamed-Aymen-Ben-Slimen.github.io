a61cd5255df44ba95a561f86eafbe093da4bc099
Mohamed-Aymen-Ben-Slimen <50459819+Mohamed-Aymen-Ben-Slimen@users.noreply.github.com>
2021-09-11 20:52:20 +0100

    expensya update

--- src/ts/Data/Experience.ts
+++ src/ts/Data/Experience.ts
@@ -4,48 +4,77 @@ import { ExperienceData } from '../Classes/Elements/Experience'
 
 export const Experience: ExperienceData[] = [
     {
+        svg: 'expensya.svg',
+        link: 'https://www.expensya.com',
+        company: 'Expensya ',
+        location: 'Part-time job - Tunisia',
+        position: 'Software Engineer',
+        begin: 'August 2021',
+        end: 'Present',
+        flavor: 'Expensya is a Web and Mobile solution that automates expense reports management for professionals.',
+        roles: [
+            'Frontend development with Angular.',
+            'Backend development with .NET framework.',
+        ]
+    },
+    {
+        svg: 'expensya.svg',
+        link: 'https://www.expensya.com',
+        company: 'Expensya ',
+        location: 'Internship - Tunisia',
+        position: 'Data Engineer',
+        begin: 'June 2021',
+        end: 'August 2021',
+        flavor: 'Expensya is a Web and Mobile solution that automates expense reports management for professionals.',
+        roles: [
+            'Data mining for OCR dataset.',
+            'Dashboard implementation for data visualization for OCR dataset.',
+            'Outliers detection.',
+        ]
+    },
+    {
         svg: 'apps4you.svg',
         link: 'https://www.apps4you.org',
         company: 'Apps4You ',
-        location: 'Remote',
+        location: 'Part-time job - Germany - Remote',
         position: 'Software Engineer',
         begin: 'October 2020',
-        end: 'Present',
+        end: 'Mai 2021',
         flavor: 'I am working part-time with the startup Apps4You on cars rental project.',
         roles: [
-            'Developing API service using Spring Boot.',
-            'Developing front-end application using Angular.',
+            'Backend development with Spring Boot framework and MongoDB.',
+            'Frontend development with Angular.',
         ]
     },
     {
         svg: 'orange.svg',
         link: 'https://www.orange.tn',
-        company: 'Orange',
-        location: 'Tunis, Tunisia',
+        company: 'Orange in collaboration with Google',
+        location: 'Internship - Tunisia',
         position: 'Web Developer Intern',
         begin: 'July 2020',
         end: 'September 2020',
         flavor: "For my summer internship, I have participated in Orange Summer Challenge program (OSC) in partnership with Google. \n"
                 + 'I was the full-stack web developer for Ubo project, a console dedicated to autism treatment.',
         roles: [
-            'Developing an API using Express.js and MongoDB.',
-            'Developing a dashboard using React.',
-            'Developing a landing page website.',
+            'Backend development with Express.js and MongoDB.',
+            'Dashboard development with React.',
+            'Landing page development.',
         ]
     },
     {
         svg: 'arsela.svg',
         link: 'https://www.arsela.co/en/',
         company: 'Arsela Technologies',
-        location: 'Remote',
-        position: 'Web Developer Developer',
+        location: 'Internship - Tunisia',
+        position: 'OCR Developer',
         begin: 'July 2020',
         end: 'Auguest 2020',
         flavor: 'Optical Document Character Recognition Project Development and their conversion into appropriate formats.',
         roles: [
-            'Developing an API using Express.js and MongoDB.',
-            'Developing an OCR microservice using Tesseract.',
-            'Developing a dashboard using React.',
+            'Backend development with Express.js and MongoDB.',
+            'OCR microservice development with Tesseract python.',
+            'Dashboard development with React.',
         ]
     },
 ]
