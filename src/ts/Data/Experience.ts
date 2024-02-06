03a594bf1763e25c81d6eee8207f3f9b6ecba809
MohamedAymenBenSlimen <mohamed.aymen.ben.slimen@gmail.com>
2024-02-06 16:12:20 +0100

    add pabolo

--- src/ts/Data/Experience.ts
+++ src/ts/Data/Experience.ts
@@ -1,127 +1,146 @@
 // Data for Experience cards
 
-import { ExperienceData } from '../Classes/Elements/Experience'
+import { ExperienceData } from "../Classes/Elements/Experience";
 
 export const Experience: ExperienceData[] = [
-    {
-        svg: 'aws.svg',
-        link: 'https://aws.amazon.com',
-        company: 'Amazon AWS',
-        location: 'Internship - Berlin, Germany',
-        position: 'Software Engineer',
-        begin: 'November 2022',
-        end: 'Present',
-        flavor: `Worked on AWS DevOps Guru which a fully managed operations service that makes it easy for
+  {
+    svg: "pabolo.svg",
+    link: "https://de.linkedin.com/company/pabolo",
+    company: "Pabolo GmbH",
+    location: "Karlsruhe, Germany",
+    position: "Software Engineer",
+    begin: "March 2023",
+    end: "Present",
+    flavor: `I contributed to build an innovative platform that empowers users to unleash their creativity as movie directors with the assistance of AI technology.`,
+    roles: [
+      "Collaborated closely with cross-functional teams to integrate cutting-edge AI technology into the platform.",
+      "Actively participated in code reviews, testing, and debugging processes to maintain high-quality standards and deliver a reliable product to users.",
+    ],
+  },
+  {
+    svg: "aws.svg",
+    link: "https://aws.amazon.com",
+    company: "Amazon AWS",
+    location: "Internship - Berlin, Germany",
+    position: "Software Engineer",
+    begin: "November 2022",
+    end: "March 2023",
+    flavor: `Worked on AWS DevOps Guru which a fully managed operations service that makes it easy for
          developers and operators to improve the performance and availability of their applications. `,
-        roles: [
-            'Build a system to improve DevOps Guru\'s availability with DevOps Guru service.',
-            'Build a system to generate tickets for on-call engineers when a problem is detected in production.',
-        ]
-    },
-    {
-        svg: 'tucan.svg',
-        link: 'https://www.tucan.ai',
-        company: 'Tucan.ai',
-        location: 'Internship - Berlin, Germany',
-        position: 'Software Engineer',
-        begin: 'February 2022',
-        end: 'September 2022',
-        flavor: `Tucan.ai is a Berlin-based tech startup developing AI software aimed at improving our professional communication.
+    roles: [
+      "Build a system to improve DevOps Guru's availability with DevOps Guru service.",
+      "Build a system to generate tickets for on-call engineers when a problem is detected in production.",
+    ],
+  },
+  {
+    svg: "tucan.svg",
+    link: "https://www.tucan.ai",
+    company: "Tucan.ai",
+    location: "Internship - Berlin, Germany",
+    position: "Software Engineer",
+    begin: "February 2022",
+    end: "September 2022",
+    flavor: `Tucan.ai is a Berlin-based tech startup developing AI software aimed at improving our professional communication.
          Tucan offers productivity tools based on automatic speech recognition,
           transcription and summarisation to help B2B customers organise, carry out and post-process meetings 
           and other structured communication more efficiently. `,
-        roles: [
-            'Build machine learning models.',
-            'Models deployment.',
-            'Fix bugs in production',
-        ]
-    },
-    {
-        svg: 'expensya.svg',
-        link: 'https://www.expensya.com',
-        company: 'Expensya ',
-        location: 'Part-time job - Tunisia',
-        position: 'Software Engineer',
-        begin: 'August 2021',
-        end: 'February 2022',
-        flavor: 'Expensya is a Web and Mobile solution that automates expense reports management for professionals.',
-        roles: [
-            'Frontend development with Angular.',
-            'Backend development with .NET framework.',
-        ]
-    },
-    {
-        svg: 'expensya.svg',
-        link: 'https://www.expensya.com',
-        company: 'Expensya ',
-        location: 'Internship - Tunisia',
-        position: 'Data Engineer',
-        begin: 'June 2021',
-        end: 'August 2021',
-        flavor: 'Expensya is a Web and Mobile solution that automates expense reports management for professionals.',
-        roles: [
-            'Data mining for OCR dataset.',
-            'Dashboard implementation for data visualization for OCR dataset.',
-            'Outliers detection.',
-        ]
-    },
-    {
-        svg: 'tplus.svg',
-        link: 'https://www.apps4you.org',
-        company: 'T+',
-        location: 'Freelance - France - Remote',
-        position: 'Software Engineer',
-        begin: 'Mai 2021',
-        end: 'Present',
-        flavor: 'I am working with T+ company on a white board web application.',
-        roles: [
-            'Backend development with Express.js framework, Web Sockets and MongoDB.',
-            'Frontend development with Angular.',
-        ]
-    },
-    {
-        svg: 'apps4you.svg',
-        link: 'https://www.apps4you.org',
-        company: 'Apps4You ',
-        location: 'Part-time job - Germany - Remote',
-        position: 'Software Engineer',
-        begin: 'October 2020',
-        end: 'Mai 2021',
-        flavor: 'I am working part-time with the startup Apps4You on cars rental project.',
-        roles: [
-            'Backend development with Spring Boot framework and MongoDB.',
-            'Frontend development with Angular.',
-        ]
-    },
-    {
-        svg: 'orange.svg',
-        link: 'https://www.orange.tn',
-        company: 'Orange in collaboration with Google',
-        location: 'Internship - Tunisia',
-        position: 'Web Developer Intern',
-        begin: 'July 2020',
-        end: 'September 2020',
-        flavor: "For my summer internship, I have participated in Orange Summer Challenge program (OSC) in partnership with Google. \n"
-                + 'I was the full-stack web developer for Ubo project, a console dedicated to autism treatment.',
-        roles: [
-            'Backend development with Express.js and MongoDB.',
-            'Dashboard development with React.',
-            'Landing page development.',
-        ]
-    },
-    {
-        svg: 'arsela.svg',
-        link: 'https://www.arsela.co/en/',
-        company: 'Arsela Technologies',
-        location: 'Internship - Tunisia',
-        position: 'OCR Developer',
-        begin: 'July 2020',
-        end: 'Auguest 2020',
-        flavor: 'Optical Document Character Recognition Project Development and their conversion into appropriate formats.',
-        roles: [
-            'Backend development with Express.js and MongoDB.',
-            'OCR microservice development with Tesseract python.',
-            'Dashboard development with React.',
-        ]
-    },
-]
+    roles: [
+      "Build machine learning models.",
+      "Models deployment.",
+      "Fix bugs in production",
+    ],
+  },
+  {
+    svg: "expensya.svg",
+    link: "https://www.expensya.com",
+    company: "Expensya ",
+    location: "Part-time job - Tunisia",
+    position: "Software Engineer",
+    begin: "August 2021",
+    end: "February 2022",
+    flavor:
+      "Expensya is a Web and Mobile solution that automates expense reports management for professionals.",
+    roles: [
+      "Frontend development with Angular.",
+      "Backend development with .NET framework.",
+    ],
+  },
+  {
+    svg: "expensya.svg",
+    link: "https://www.expensya.com",
+    company: "Expensya ",
+    location: "Internship - Tunisia",
+    position: "Data Engineer",
+    begin: "June 2021",
+    end: "August 2021",
+    flavor:
+      "Expensya is a Web and Mobile solution that automates expense reports management for professionals.",
+    roles: [
+      "Data mining for OCR dataset.",
+      "Dashboard implementation for data visualization for OCR dataset.",
+      "Outliers detection.",
+    ],
+  },
+  {
+    svg: "tplus.svg",
+    link: "https://www.apps4you.org",
+    company: "T+",
+    location: "Freelance - France - Remote",
+    position: "Software Engineer",
+    begin: "Mai 2021",
+    end: "Present",
+    flavor: "I am working with T+ company on a white board web application.",
+    roles: [
+      "Backend development with Express.js framework, Web Sockets and MongoDB.",
+      "Frontend development with Angular.",
+    ],
+  },
+  {
+    svg: "apps4you.svg",
+    link: "https://www.apps4you.org",
+    company: "Apps4You ",
+    location: "Part-time job - Germany - Remote",
+    position: "Software Engineer",
+    begin: "October 2020",
+    end: "Mai 2021",
+    flavor:
+      "I am working part-time with the startup Apps4You on cars rental project.",
+    roles: [
+      "Backend development with Spring Boot framework and MongoDB.",
+      "Frontend development with Angular.",
+    ],
+  },
+  {
+    svg: "orange.svg",
+    link: "https://www.orange.tn",
+    company: "Orange in collaboration with Google",
+    location: "Internship - Tunisia",
+    position: "Web Developer Intern",
+    begin: "July 2020",
+    end: "September 2020",
+    flavor:
+      "For my summer internship, I have participated in Orange Summer Challenge program (OSC) in partnership with Google. \n" +
+      "I was the full-stack web developer for Ubo project, a console dedicated to autism treatment.",
+    roles: [
+      "Backend development with Express.js and MongoDB.",
+      "Dashboard development with React.",
+      "Landing page development.",
+    ],
+  },
+  {
+    svg: "arsela.svg",
+    link: "https://www.arsela.co/en/",
+    company: "Arsela Technologies",
+    location: "Internship - Tunisia",
+    position: "OCR Developer",
+    begin: "July 2020",
+    end: "Auguest 2020",
+    flavor:
+      "Optical Document Character Recognition Project Development and their conversion into appropriate formats.",
+    roles: [
+      "Backend development with Express.js and MongoDB.",
+      "OCR microservice development with Tesseract python.",
+      "Dashboard development with React.",
+    ],
+  },
+];
