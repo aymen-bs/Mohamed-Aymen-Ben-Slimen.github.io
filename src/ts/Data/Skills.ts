fa0170ae40fe85670b25c091faca0f978d64b7c3
Mohamed Aymen Ben Slimen <mohamed.aymen.ben.slimen@gmail.com>
2025-01-12 13:34:35 +0100

    add docker and kubernetes to skills

--- src/ts/Data/Skills.ts
+++ src/ts/Data/Skills.ts
@@ -1,138 +1,154 @@
 // Data for all Skills
 
-import { SkillData, SkillCategory } from '../Classes/Elements/Skill'
+import { SkillData, SkillCategory } from "../Classes/Elements/Skill";
 
 export const Skills: SkillData[] = [
-    {
-        name: 'JavaScript',
-        svg: 'javascript',
-        color: '#F0DB4F',
-        category: SkillCategory.Web | SkillCategory.Programming,
-    },
-    {
-        name: 'TypeScript',
-        svg: 'typescript',
-        color: '#007ACC',
-        category: SkillCategory.Web | SkillCategory.Programming,
-    },
-    {
-        name: 'Node.js',
-        svg: 'nodejs',
-        color: '#8CC84B',
-        category: SkillCategory.Programming | SkillCategory.Server,
-    },
-    {
-        name: 'Angular',
-        svg: 'angular',
-        color: '#DD0031',
-        category: SkillCategory.Web | SkillCategory.Framework,
-    },
-    {
-        name: 'React',
-        svg: 'react',
-        color: '#00D8FF',
-        category: SkillCategory.Web | SkillCategory.Framework,
-    },
-    {
-        name: 'Nest.js',
-        svg: 'nestjs',
-        color: '#F0DB4F',
-        category: SkillCategory.Web | SkillCategory.Framework,
-    },
-    {
-        name: 'C++',
-        svg: 'cplusplus',
-        color: '#9B023A',
-        category: SkillCategory.Programming | SkillCategory.Server,
-    },
-    {
-        name: 'HTML',
-        svg: 'html',
-        color: '#EF652A',
-        category: SkillCategory.Web,
-    },
-    {
-        name: 'CSS',
-        svg: 'css',
-        color: '#3C9CD7',
-        category: SkillCategory.Web,
-    },
-    {
-        name: 'SASS/SCSS',
-        svg: 'sass',
-        color: '#CD669A',
-        category: SkillCategory.Web,
-    },
-    {
-        name: 'Express JS',
-        svg: 'express',
-        color: '#3D3D3D',
-        category: SkillCategory.Server | SkillCategory.Framework,
-    },
-    {
-        name: 'MongoDB',
-        svg: 'mongodb',
-        color: '#d44000',
-        category: SkillCategory.Database,
-    },
-    {
-        name: 'Firebase',
-        svg: 'firebase',
-        color: '#FFCA28',
-        category: SkillCategory.Database,
-    },
-    {
-        name: 'TensorFlow',
-        svg: 'tensorflow',
-        color: '#5557ff',
-        category: SkillCategory.DataScience,
-    },
-    {
-        name: 'PyTorch',
-        svg: 'pytorch',
-        color: '#2B3539',
-        category: SkillCategory.DataScience,
-    },
-    {
-        name: 'Git',
-        svg: 'git',
-        color: '#F05032',
-        category: SkillCategory.Programming,
-    },
-    {
-        name: 'GNU Bash',
-        svg: 'bash',
-        color: '#2B3539',
-        category: SkillCategory.Scripting,
-    },
-    {
-        name: 'Java',
-        svg: 'java',
-        color: '#007699',
-        category: SkillCategory.Programming | SkillCategory.Server,
-    },
-    {
-        name: 'Python',
-        svg: 'python',
-        color: '#3776AB',
-        category: SkillCategory.Programming | SkillCategory.Scripting | SkillCategory.Server,
-    },
-    {
-        name: 'Spring',
-        svg: 'spring',
-        color: '#6DB33F',
-        category: SkillCategory.Framework | SkillCategory.Server,
-    },
-    {
-        name: 'SQL',
-        svg: 'sql',
-        color: '#F89700',
-        category: SkillCategory.Database,
-    },
-    {
-        name: 'AWS',
-        svg: 'aws',
-        color: '#3D3D3D',
-        category: SkillCategory.DevOps | SkillCategory.Programming,
-    },
+  {
+    name: "JavaScript",
+    svg: "javascript",
+    color: "#F0DB4F",
+    category: SkillCategory.Web | SkillCategory.Programming,
+  },
+  {
+    name: "TypeScript",
+    svg: "typescript",
+    color: "#007ACC",
+    category: SkillCategory.Web | SkillCategory.Programming,
+  },
+  {
+    name: "Node.js",
+    svg: "nodejs",
+    color: "#8CC84B",
+    category: SkillCategory.Programming | SkillCategory.Server,
+  },
+
+  {
+    name: "React",
+    svg: "react",
+    color: "#00D8FF",
+    category: SkillCategory.Web | SkillCategory.Framework,
+  },
+  {
+    name: "Angular",
+    svg: "angular",
+    color: "#DD0031",
+    category: SkillCategory.Web | SkillCategory.Framework,
+  },
+  {
+    name: "Nest.js",
+    svg: "nestjs",
+    color: "#F0DB4F",
+    category: SkillCategory.Web | SkillCategory.Framework,
+  },
+  {
+    name: "Kubernetes",
+    svg: "kubernetes",
+    color: "#326CE5",
+    category: SkillCategory.DevOps,
+  },
+  {
+    name: "Docker",
+    svg: "docker",
+    color: "#1d63ed",
+    category: SkillCategory.DevOps,
+  },
+  {
+    name: "C++",
+    svg: "cplusplus",
+    color: "#9B023A",
+    category: SkillCategory.Programming | SkillCategory.Server,
+  },
+  {
+    name: "HTML",
+    svg: "html",
+    color: "#EF652A",
+    category: SkillCategory.Web,
+  },
+  {
+    name: "CSS",
+    svg: "css",
+    color: "#3C9CD7",
+    category: SkillCategory.Web,
+  },
+  {
+    name: "SASS/SCSS",
+    svg: "sass",
+    color: "#CD669A",
+    category: SkillCategory.Web,
+  },
+  {
+    name: "Express JS",
+    svg: "express",
+    color: "#3D3D3D",
+    category: SkillCategory.Server | SkillCategory.Framework,
+  },
+  {
+    name: "MongoDB",
+    svg: "mongodb",
+    color: "#d44000",
+    category: SkillCategory.Database,
+  },
+  {
+    name: "Firebase",
+    svg: "firebase",
+    color: "#FFCA28",
+    category: SkillCategory.Database,
+  },
+  {
+    name: "TensorFlow",
+    svg: "tensorflow",
+    color: "#5557ff",
+    category: SkillCategory.DataScience,
+  },
+  {
+    name: "PyTorch",
+    svg: "pytorch",
+    color: "#2B3539",
+    category: SkillCategory.DataScience,
+  },
+  {
+    name: "Git",
+    svg: "git",
+    color: "#F05032",
+    category: SkillCategory.Programming,
+  },
+  {
+    name: "GNU Bash",
+    svg: "bash",
+    color: "#2B3539",
+    category: SkillCategory.Scripting,
+  },
+  {
+    name: "Java",
+    svg: "java",
+    color: "#007699",
+    category: SkillCategory.Programming | SkillCategory.Server,
+  },
+  {
+    name: "Python",
+    svg: "python",
+    color: "#3776AB",
+    category:
+      SkillCategory.Programming |
+      SkillCategory.Scripting |
+      SkillCategory.Server,
+  },
+  {
+    name: "Spring",
+    svg: "spring",
+    color: "#6DB33F",
+    category: SkillCategory.Framework | SkillCategory.Server,
+  },
+  {
+    name: "SQL",
+    svg: "sql",
+    color: "#F89700",
+    category: SkillCategory.Database,
+  },
+  {
+    name: "AWS",
+    svg: "aws",
+    color: "#3D3D3D",
+    category: SkillCategory.DevOps | SkillCategory.Programming,
+  },
 ];
